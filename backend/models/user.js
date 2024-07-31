4ba0a0857110b15623e7f0325da637a775de4e8a
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-07-31 17:38:37 -0400

    jwt added activity complete

--- backend/models/user.js
+++ backend/models/user.js
@@ -21,9 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     passwordDigest: DataTypes.STRING
-  },
-
-    {
+  }, {
     sequelize,
     underscored: true,
     modelName: 'User',
